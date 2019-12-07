a53f62dae41f85a2e85b4c30219a018393b6742e
muminosman <muminoth@gmail.com>
2019-12-07 15:13:13 +0300

    Updates

---
+++ precache-manifest.6773e7a002aae0dc4330b489c8ee672a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21799bccd847f35e5d44b6ff0ef6322d",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "7da0a9e8f935b5d7a382",
+    "url": "/burger-builder/static/css/main.d4cfcd24.chunk.css"
+  },
+  {
+    "revision": "9ef3741ee4e0f8703fd6",
+    "url": "/burger-builder/static/js/2.ce46dec1.chunk.js"
+  },
+  {
+    "revision": "7da0a9e8f935b5d7a382",
+    "url": "/burger-builder/static/js/main.30a6ba6a.chunk.js"
+  },
+  {
+    "revision": "f26687a28f5dfa8916ba",
+    "url": "/burger-builder/static/js/runtime-main.a70e4314.js"
+  }
+]);
